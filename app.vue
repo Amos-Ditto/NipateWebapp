c6def8bbabeb38af62ce71d405b00912fc4e00b0
Amos Kipyegon <amoskip@kabarak.ac.ke>
2022-06-10 08:39:32 +0300

    splash page refix

--- app.vue
+++ app.vue
@@ -35,171 +35,61 @@ body {
   overflow-x: hidden;
 }
 
-.flex {
-  display: flex;
-}
-.justify-center {
-  justify-content: center;
-}
-.justify-right {
-  justify-content: flex-end;
-}
-.justify-align-center {
-  justify-content: center;
-  align-items: center;
-}
-.align-center {
-  align-items: center;
-}
-.flex-row {
-  flex-direction: row;
-}
-.flex-column {
-  flex-direction: column;
-}
-.flex-wrap {
-  flex-wrap: wrap;
-}
-.wh-100 {
-  width: 100%;
-  height: 100%;
-}
-.flex-between {
-  justify-content: space-between;
-}
-.w-100 {
-  width: 100%;
-}
-.w-50 {
-  width: 50%;
-}
-.h-100 {
-  height: 100%;
-}
-.h-50 {
-  height: 50%;
-}
-.bg-inherit {
-  background: inherit;
-}
-.font-12 {
-  font-size: 12px;
-}
-.font-14 {
-  font-size: .9rem;
-}
-.font-16 {
-  font-size: 16px;
-}
-.font-18 {
-  font-size: 18px;
-}
-.font-20 {
-  font-size: 20px;
-}
-.font-24 {
-  font-size: 24px;
-}
-.f-weight-500 {
-  font-weight: 500;
-}
-.f-weight-900 {
-  font-weight: 900;
-}
-.padding-2 {
-  padding: 2px;
-}
-.padding-4 {
-  padding: 4px;
-}
-.padding-8 {
-  padding: 8px;
-}
-.padding-12 {
-  padding: 12px;
-}
-.padding-w-4 {
-  padding: 0 4px;
-}
-.padding-w-8 {
-  padding: 0 8px;
-}
-.gap--5 {
-  gap: 0.2rem;
-}
-.gap-1 {
-  gap: 1rem;
-}
-.gap-2 {
-  gap: 2rem;
-}
-.relative {
-  position: relative;
-}
-.absolute {
-  position: absolute;
-}
-.color-white {
-  color: var(--color-white);
-}
-.color-variant-dark {
-  color: var(--color-variant-black);
-}
-.c-dark {
-  color: var(--color-dark);
-}
-.left-1 {
-  margin-left: 1rem;
-}
-.left-3 {
-  margin-left: 3rem;
-}
-.top-3 {
-  margin-top: 3rem;
-}
-.border-4 {
-  border-radius: 4px;
-}
-.b-4 {
-  border-radius: 4px;
-}
-.b-8 {
-  border-radius: 8px;
-}
-.br-12 {
-  border-radius: 12px;
-}
-.btn-b-none {
-  border: none;
-  outline: none;
-}
-.btn-b-4 {
-  border-radius: 4px;
-}
-.btn-b-8 {
-  border-radius: 8px;
-}
-.bg-gold {
-  background: var(--color-gold);
-}
-.w-2 {
-  width: 20%;
-}
-.w-3 {
-  width: 30%;
-}
-.w-4 {
-  width: 40%;
-}
-.w-6 {
-  width: 60%;
-}
-.w-7 {
-  width: 70%;
-}
-.w-8 {
-  width: 80%;
-}
+.flex {display: flex;}
+.justify-center {justify-content: center;}
+.justify-right {justify-content: flex-end;}
+.justify-align-center {justify-content: center;align-items: center;}
+.align-center {align-items: center;}
+.flex-row {flex-direction: row;}
+.flex-column {flex-direction: column;}
+.flex-wrap {flex-wrap: wrap;}
+.wh-100 {width: 100%;height: 100%;}
+.flex-between {justify-content: space-between;}
+.w-100 {width: 100%;}
+.w-50 {width: 50%;}
+.h-100 {height: 100%;}
+.h-50 {height: 50%;}
+.bg-inherit {background: inherit;}
+.font-12 {font-size: 12px;}
+.font-14 {font-size: .9rem;}
+.font-16 {font-size: 16px;}
+.font-18 {font-size: 18px;}
+.font-20 {font-size: 20px;}
+.font-24 {font-size: 24px;}
+.f-weight-500 {font-weight: 500;}
+.f-weight-900 {font-weight: 900;}
+.padding-2 {padding: 2px;}
+.padding-4 {padding: 4px;}
+.padding-8 {padding: 8px;}
+.padding-12 {padding: 12px;}
+.padding-w-4 {padding: 0 4px;}
+.padding-w-8 {padding: 0 8px;}
+.gap--5 {gap: 0.2rem;}
+.gap-1 {gap: 1rem;}
+.gap-2 {gap: 2rem;}
+.relative {position: relative;}
+.absolute {position: absolute;}
+.color-white {color: var(--color-white);}
+.color-variant-dark {color: var(--color-variant-black);}
+.c-dark {color: var(--color-dark);}
+.left-1 {margin-left: 1rem;}
+.left-3 {margin-left: 3rem;}
+.top-3 {margin-top: 3rem;}
+.border-4 {border-radius: 4px;}
+.b-4 {border-radius: 4px;}
+.b-8 {border-radius: 8px;}
+.br-12 {border-radius: 12px;}
+.btn-b-none {border: none; outline: none;}
+.btn-b-4 {border-radius: 4px;}
+.btn-b-8 {border-radius: 8px;}
+.bg-gold {background: var(--color-gold);}
+.w-20 {width: 20%;}
+.w-30 {width: 30%;}
+.w-40 {width: 40%;}
+.w-60 {width: 60%;}
+.w-70 {width: 70%;}
+.w-80 {width: 80%;}
+.font-30 {font-size: 30px;}
 #top-navbar {
   width: 100vw;
   height: 1rem;
--- pages/index.vue
+++ pages/index.vue
@@ -6,20 +6,17 @@
             </template>
             <section class="w-100 padding-8">
                 <div class="background-img"></div>
-                <div class="splash-context w-100 flex flex-column justify-align-center">
-                    <div class="splash-content w-100 flex justify-align-center">
-                        <div class="splash-text w-4 font-20 w-4">
-                            <h3 class="">Powerful and easy to use platform</h3>
-                        </div>
-                        <div class="flex justify-align-center w-6">
-                            <img src="@/assets/logo-img-removebg-preview.png" alt="" class="bg-inherit w-100">
-                        </div>
+                <main class="flex justify-align-center w-100 flex-wrap">
+                    <div class="splash-title flex align-center w-80 flex-wrap">
+                        <h3 class="c-dark flex flex-wrap w-100 top-3">
+                            Powerful and easy to use platform.
+                        </h3>
                     </div>
-                    <div class="flex align-center w-100 flex-row gap-2 top-3 padding-w-8">
-                        <button class="started bg-gold color-white btn-started br-12 btn-b-none f-weight-900 font-18 padding-12">Get Started</button>
-                        <button class="contact font-20 bg-inherit btn-b-none flex align-center flex-row gap--5 padding-8">Contactus<div class="i-mdi-arrow-forward" /></button>
+                    <div class="splash-continue flex flex-row justify-align-center w-100 gap-2 top-3">
+                        <button class="padding-12 font-18 btn-b-none bg-gold br-12">Get Started</button>
+                        <button class="padding-12 flex flex-row font-16 b-4 bg-inherit br-12">Contact Us <div class="i-mdi-chevron-right font-20"></div></button>
                     </div>
-                </div>
+                </main>
             </section>
         </NuxtLayout>
     </main>
@@ -50,19 +47,24 @@
     background-position: center;
     background-size: cover;
 }
-.splash-context {
-    height: 25rem;
-    // background: #abc;
-}
-.splash-content {
-    img {
-        object-fit: cover;
-    }
-}
+
 button {
     transition: transform .3s;
     &:hover {
         transform: scaleX(1.12);
+    }
+}
+.splash-title {
+    h3 {
+        font-size: 45px;
+        letter-spacing: 2px;
+    }
+}
+.splash-continue {
+    button {
+        &:last-child {
+            border: 1px solid var(--color-variant-black);
+        }
     }
 }
 @media screen and (min-width: 600px) {
@@ -70,6 +72,15 @@ button {
         aspect-ratio: 600/400;
         top: 5rem;
         background-image: url('@/assets/blob-haikei-full.svg');
+    }
+    main {
+        width: 75%;
+        gap: 3rem;
+        .splash-continue {
+            justify-content: center;
+            width: 50%;
+            margin-right: 8rem;
+        }
     }
 }
 </style>
