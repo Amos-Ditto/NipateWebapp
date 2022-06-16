74c6c84dc2e1ce1045fca505eae43b05ee7daf56
tinegaCollins <collinstinega003@gmail.com>
2022-06-16 10:17:11 +0300

    adding meta data to app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,4 +10,22 @@ export default defineNuxtConfig({
       presetIcons(),
     ],
   },
+  app: {
+    head: {
+      title: "nipate",
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+      script: [
+        //{ src: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.js' } hii ni examl
+      ],
+      link: [
+        // { rel: 'icon', href: './assets/like-thumb-up-svgrepo-com.svg' } hii pia
+      ],
+      // please note that this is an area that is likely to change
+      style: [
+        // <style type="text/css">:root { color: red }</style>
+      ]
+    },
+  }
 })
