b894f6215952b39f247dc40be024fe46ea350d65
Amos Kipyegon <amoskip@kabarak.ac.ke>
2022-07-17 00:33:36 +0300

    ✅ services request layout

---
+++ components/Cards/RequestListCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <main class="list-card w-full min-h-[120px] shadow bg-white px-2 md:px-6 py-1 md:py-3 flex flex-col gap-1 md:gap-3 rounded">
+        <div class="top w-full flex flex-row justify-between items-center">
+            <h3 class="text-xs font-sans text-neutral-500 tracking-wide">Commutation & Transport</h3>
+            <span class="text-xs text-neutral-400">30 mins ago</span>
+        </div>
+        <div class="product w-full px-2">
+            <h3 class="text-neutral-500 text-xl font-bold">Boda boda</h3>
+        </div>
+        <div class="icons flex flex-row w-full items-center text-neutral-400 text-xs gap-8 px-4">
+            <div class="user flex flex-row items-center gap-1">
+                <div class="i-mdi-user" />
+                Amos
+            </div>
+            <div class="location flex flex-row items-center gap-1">
+                <div class="i-mdi-map-marker" />
+                Nakuru - Kabarak
+            </div>
+        </div>
+        <div class="description w-full leading-normal text-neutral-500 text-sm indent-2">
+            <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis repellendus tenetur optio quia et? Nesciunt quidem <span class="text-orange-500 text-xs">...read more</span> </h3>
+        </div>
+        <div class="btns flex flex-row w-full items-center py-2">
+            <button class="bg-orange-400 hover:bg-orange-500 text-slate-50 px-3 font-bold rounded tracking-wider">view</button>
+        </div>
+    </main>
+</template>
--- pages/index/services.vue
+++ pages/index/services.vue
@@ -1,82 +1,44 @@
 <script setup lang="ts">
 import ServicesList from "../../components/context/ServicesList.vue";
 
-const findlocation = ref<boolean>(false);
-const findservices = ref<boolean>(false);
+const selectservices = ref<boolean>(false);
 
-// Methods
-const filterLocation = ():void => {
-    if (findservices.value == true) {
-        findservices.value = false;
-    };
-    setTimeout(() => {
-        findlocation.value = !findlocation.value;
-    }, 200);
-}
-const filterServices = ():void => {
-    if (findlocation.value == true) {
-        findlocation.value = false;
-    };
-    setTimeout(() => {
-        findservices.value = !findservices.value;
-    }, 200);
-}
-const closeServiceSearch = ():void => {
-    findservices.value = false;
-};
-const closeLocationSearch = ():void => {
-    findlocation.value = false;
-}
 </script>
 <template>
     <main class="w-full flex flex-col py-1 gap-2 overflow-y-auto relative min-h-[80vh]">
-        <div class="navigation-bar w-full flex flex-row justify-between">
-            <div class="filters flex items-center">
-                <button class="flex flex-row items-center text-neutral-600 text-sm gap-2 px-2 font-bold hover:text-green-600">
-                    Find by <div class="i-mdi-filter-variant" />
-                </button>
-            </div>
+        <div class="navigation-bar w-full flex flex-row justify-end md:justify-start">
             <div class="links flex flex-row gap-2 md:gap-8">
                 <button>My requests</button>
                 <button>Request service</button>
             </div>
         </div>
         <hr>
-        <div class="filter-btns flex flex-row px-3 gap-3 w-full md:w-auto">
-            <div class="relative filter-btn">
-                <button
-                    @click="filterLocation"
-                    :class="findlocation ? 'border-green-500 text-green-500' : 'text-neutral-500 border-neutral-300'"
-                >
-                    Location <div class="i-mdi-map-marker" />
+        <section class="w-full flex flex-row justify-evenly">
+            
+            <section class="flex flex-row gap-4 w-full md:w-[75%]">
+                <div class="services-list w-full">
+                    <ServicesList />
+                </div>
+            </section>
+            <div class="filters-tags hidden md:flex flex-col w-[20%] py-3">
+                <button class="flex flex-row items-center text-neutral-600 text-lg italic gap-3 px-2 font-bold hover:text-green-600">
+                    Find by <div class="i-mdi-filter-variant" />
                 </button>
-                <DropdownsMenuDropDown v-if="findlocation" @close-location-search="closeLocationSearch" />
-            </div>
-            <div class="relative filter-btn">
-                <button
-                    :class="findservices ? 'border-green-500 text-green-500' : 'text-neutral-500 border-neutral-300'"
-                    @click="filterServices"
-                >
-                    services <div class="i-mdi-account-wrench" /></button>
-                <DropdownsServicesDropDown v-if="findservices" @close-service-search="closeServiceSearch" />
-            </div>
-        </div>
-        <section class="pt-3 md:pt-4 flex w-full flex-row gap-4">
-            <div class="services-list w-full sm:w-[80%] md:w-[60%]">
-                <ServicesList />
-            </div>
-            <div class="request-stats hidden sm:block w-[30%]">
-
+                <div class="filter-tags-group flex flex-col w-full pt-4 px-4 gap-2">
+                    <LazyFiltersLocationFilter />
+                    <LazyFiltersServiceFilters />
+                </div>
             </div>
         </section>
     </main>
 </template>
 
 <style scoped>
+
 .navigation-bar {
     @apply pr-2 md:pr-16 pt-3 md:pt-0;
 }
-.filters , .filter-btns button , .navigation-bar .links button{
+.filters-tags , .navigation-bar .links button{
     font-family: "NonBreakingSpaceOverride","Hoefler Text",Garamond,"Times New Roman",serif;
 }
 
@@ -84,15 +46,22 @@ const closeLocationSearch = ():void => {
     @apply px-2 text-neutral-600 text-sm sm:text-base md:text-lg font-bold tracking-wide hover:text-green-500;
     transition: color 250ms ease;
 }
-.filter-btns button {
-    @apply flex flex-row items-center gap-1.5 text-sm md:text-base border px-2 md:px-4 py-1 rounded;
-    @apply hover:border-green-500 hover:text-green-500;
-    transition: border 300ms ease , color 300ms ease;
+.tag-values label {
+    @apply w-full flex flex-row items-center text-neutral-500;
+    @apply text-sm tracking-wide hover:bg-slate-100 px-2 py-2 cursor-pointer;
+    display: grid;
+    grid-template-columns: 1em auto;
+    gap: 0.5em;
 }
-.filter-btns {
-    @apply md:absolute md:top-0 md:translate-x-[40%];
+.tag-values button {
+    @apply w-full flex items-center text-start text-neutral-500;
+    @apply text-base hover:bg-slate-100 py-2 px-2 cursor-pointer;
 }
-/* .filter-btn .dropdown {
-    @apply absolute bottom-0 translate-y-[110%] min-w-[200px] min-h-[150px] bg-slate-100 rounded border border-neutral-300;
-} */
+.tag-values label input {
+    @apply w-[1rem] h-[1rem];
+}
+.search-btn button {
+    @apply bg-blue-400 text-white font-bold px-8 tracking-wide rounded hover:bg-blue-600;
+    transition: background-color 300ms ease;
+}
 </style>
