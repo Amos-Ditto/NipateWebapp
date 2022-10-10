a85534dc7b8ff534eb1bbec194f3c2da0c42a70c
Amos Kipyegon <amoskip@kabarak.ac.ke>
2022-10-10 09:17:27 +0300

    chore:  Update Home Dashboard

--- src/components/Layouts/MainDropDown.vue
+++ src/components/Layouts/MainDropDown.vue
@@ -2,22 +2,16 @@
     <div class="w-full px-1 grid grid-cols-2 gap-x-4 sm:gap-x-8">
         <div class="services-links flex flex-col gap-y-3">
             <div class="title w-full flex flex-row items-center gap-x-1">
-                <h3 class="hover:text-steelblue text-sm sm:text-lg font-bold tracking-wide transition-colors duration-300 cursor-pointer">Services</h3>
-                <div class="i-mdi-chevron-down"></div>
+                <h3 class="text-sm sm:text-lg font-bold tracking-wide transition-colors duration-300 cursor-pointer">Services</h3>
+                <div class="i-mdi-chevron-down transition-transform duration-300"></div>
             </div>
         </div>
         <div class="regions-links flex flex-col gap-y-3">
             <div class="title w-full flex flex-row items-center gap-x-1">
-                <h3 class="hover:text-steelblue text-sm sm:text-lg font-bold tracking-wide transition-colors duration-300 cursor-pointer">Regions</h3>
-                <div class="i-mdi-chevron-down"></div>
+                <h3 class="text-sm sm:text-lg font-bold tracking-wide transition-colors duration-300 cursor-pointer">Regions</h3>
+                <div class="i-mdi-chevron-down transition-transform duration-300"></div>
             </div>
         </div>
-        <!-- <div class="account-links flex flex-col gap-y-3">
-            <div class="title w-full flex flex-row items-center gap-x-1">
-                <h3 class="hover:text-steelblue text-lg font-bold tracking-wide transition-colors duration-300 hover:underline cursor-pointer">Services</h3>
-                <div class="i-mdi-chevron-down"></div>
-            </div>
-        </div> -->
     </div>
 </template>
 
@@ -25,5 +19,11 @@
 .services-links ul li,
 .other-links ul li {
     @apply text-sm sm:text-base font-extralight hover:underline cursor-pointer tracking-wide;
+}
+.title:hover h3 {
+    @apply text-steelblue;
+}
+.title:hover .i-mdi-chevron-down {
+    @apply rotate-180;
 }
 </style>
---
+++ src/views/Home/SearchView.vue
@@ -0,0 +1,5 @@
+<template>
+    <section>
+        search
+    </section>
+</template>
