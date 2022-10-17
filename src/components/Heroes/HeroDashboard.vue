8df70e03aa128dbb6a3a7b06c9f44f982616f30d
Amos Kipyegon <amoskip@kabarak.ac.ke>
2022-10-17 10:03:02 +0300

    chore: Fix UI Bug

--- src/components/Heroes/HeroDashboard.vue
+++ src/components/Heroes/HeroDashboard.vue
@@ -7,10 +7,10 @@ import { ref } from 'vue';
     <div class="w-full py-2 sm:py-5 flex flex-col justify-center items-center gap-y-12 sm:gap-y-16">
         <div class="hero-title flex w-full justify-center items-center">
             <h3
-                class="text-center leading-[1.325] sm:leading-[1.25] text-3xl xs:text-5xl sm:text-5xl md:text-6xl lg:text-7xl bg-clip-text text-transparent bg-gradient-to-bl from-[#014451] to-[#1f7794] font-bold tracking-wide transition-font duration-300"
+                class="text-center leading-[1.325] sm:leading-[1.25] text-4xl xs:text-5xl sm:text-5xl md:text-6xl lg:text-7xl bg-clip-text text-transparent bg-gradient-to-bl from-[#014451] to-[#1f7794] font-bold tracking-wide transition-font duration-300"
             >Find the best people <br class="" /> to serve you.</h3>
         </div>
-        <div class="serch-container grid grid-cols-4 gap-x-2 sm:gap-x-5 w-[86%] sm:w-[65%] lg:w-[50%]">
+        <div class="serch-container grid grid-cols-4 gap-x-2 sm:gap-x-5 w-[84%] sm:w-[65%] lg:w-[50%]">
             <div class="search-input col-span-3 relative flex items-center justify-center">
                 <input type="text" placeholder="Search for service...">
                 <div class="i-mdi-magnify absolute left-3 scale-125 text-base text-slate-400"></div>
